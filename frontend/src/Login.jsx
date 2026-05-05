b1b71fcd4cec10b01a53d90c9e66a32dcad433e4
Suvethareththinam Kithurshan <163143558+Kithurshan@users.noreply.github.com>
2026-05-05 22:10:42 +0530

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import './auth.css';
 import API_URL from './config';
 
+console.log("API_URL:", API_URL);
+
 function Login({ onLoginSuccess, onGuestAccess, onNavigateRegister }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
