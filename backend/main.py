10838fc3e14010f718ab843040e8e3d1aa34bbe6
Suvethareththinam Kithurshan <163143558+Kithurshan@users.noreply.github.com>
2026-05-05 22:22:16 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,8 +31,14 @@ app = FastAPI(
     version="2.0.0"
 )
 
+origins = [
+    "http://localhost:5173",
+    "https://bank-assist-austcu31h-skkithu2003-gmailcoms-projects.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=origins,
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
